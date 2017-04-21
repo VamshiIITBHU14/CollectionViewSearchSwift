b97bb4f9f818ea90e996c31b82d82c57ad1f46b1
Vamshi Krishna <VamshiKrishna@Vamshis-MacBook-Air-2.local>
2017-04-21 08:26:36 +0530

    Search bar added!

--- CollectionViewSearchSwift/ViewController.swift
+++ CollectionViewSearchSwift/ViewController.swift
@@ -6,17 +6,19 @@
 //  Copyright © 2017 VamshiKrishna. All rights reserved.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout , TapCellDelegate, UISearchBarDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionSearchBar: UISearchBar!
     var cellIdentifier = "Cell"
     var numberOfItemsPerRow : Int = 2
     var dataSource:[String]?
     var dataSourceForSearchResult:[String]?
     var searchBarActive:Bool = false
     var searchBarBoundsY:CGFloat?
-    var searchBar:UISearchBar?
     var refreshControl:UIRefreshControl?
     
     var cellWidth:CGFloat{
@@ -26,33 +28,11 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+        collectionSearchBar.delegate = self
         collectionView.dataSource = self
         collectionView.delegate = self
-        self.dataSource = ["Modesto","Rebecka","Andria","Sergio","Robby","Jacob","Lavera", "Theola", "Adella","Garry", "Lawanda", "Christiana", "Billy", "Claretta", "Gina", "Edna", "Antoinette", "Shantae", "Jeniffer", "Fred", "Phylis", "Raymon", "Brenna", "Gulfs", "Ethan", "Kimbery", "Sunday", "Darrin", "Ruby", "Babette", "Latrisha", "Dewey", "Della", "Dylan", "Francina", "Boyd", "Willette", "Mitsuko", "Evan", "Dagmar", "Cecille", "Doug",
-                           "Jackeline", "Yolanda", "Patsy", "Haley", "Isaura", "Tommye", "Katherine", "Vivian"]
+        self.dataSource = ["Afghanistan","Albania","Algeria","Andorra","Barbados","Belarus","Belgium","Cabo Verde","Central African Republic", "Chad","Chile","Denmark","Djibouti","Eritrea","Estonia","Fiji", "Finland","Haiti","Holy See","Honduras","Iceland","India", "Japan", "Kosovo","Kuwait","Lesotho", "Liberia","Mauritius", "Mexico","Micronesia","Moldova","Namibia","Nauru","Nepal","Oman","Paraguay", "Peru","Philippines","Poland","Qatar","Russia","Senegal","Serbia","Seychelles","Sierra Leone","Tonga","Trinidad and Tobago","Tunisia","United Kingdom","Uruguay","Venezuela","Vietnam","Yemen","Zimbabwe"]
         self.dataSourceForSearchResult = [String]()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.prepareUI()
-    }
-    
-    deinit{
-        self.removeObservers()
-    }
-    
-    // MARK: actions
-    func refreshControlAction(){
-        self.cancelSearching()
-        
-//        let delayTime = DispatchTime.now(dispatch_time_t(DISPATCH_TIME_NOW), Int64(1 * Double(NSEC_PER_SEC)))
-//        dispatch_after(delayTime, dispatch_get_main_queue()) {
-//            // stop refreshing after 2 seconds
-//            self.collectionView?.reloadData()
-//            self.refreshControl?.endRefreshing()
-//        }
-     
     }
     
     // MARK: Search
@@ -85,17 +65,17 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
     }
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        self.searchBar!.setShowsCancelButton(true, animated: true)
+        self.collectionSearchBar.setShowsCancelButton(true, animated: true)
     }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
         self.searchBarActive = false
-        self.searchBar!.setShowsCancelButton(false, animated: false)
+        self.collectionSearchBar.setShowsCancelButton(false, animated: false)
     }
     func cancelSearching(){
         self.searchBarActive = false
-        self.searchBar!.resignFirstResponder()
-        self.searchBar!.text = ""
+        self.collectionSearchBar.resignFirstResponder()
+        self.collectionSearchBar.text = ""
     }
     
     
@@ -108,7 +88,10 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CollectionViewCell
         cell.indexPath = indexPath
         cell.delegate = self
-        cell.topButton.backgroundColor = UIColor .white
+        cell.topButton.backgroundColor = UIColor .blue
+        cell.layer.borderWidth = 1.0;
+        cell.layer.borderColor = UIColor.green.cgColor
+        cell.topLabel.textColor = UIColor.white
         
         if (self.searchBarActive) {
             cell.topLabel!.text = self.dataSourceForSearchResult![indexPath.row];
@@ -139,84 +122,13 @@ class ViewController: UIViewController,UICollectionViewDataSource, UICollectionV
             + (flowLayout.minimumInteritemSpacing * CGFloat(numberOfItemsPerRow - 1))
         let size = Int((collectionView.bounds.width - totalSpace) / CGFloat(numberOfItemsPerRow))
         return CGSize(width: size, height: size)
-        
     }
-    
-    func collectionView( _ collectionView: UICollectionView,
-                         layout collectionViewLayout: UICollectionViewLayout,
-                         insetForSectionAt section: Int) -> UIEdgeInsets{
-        return UIEdgeInsetsMake(self.searchBar!.frame.size.height, 0, 0, 0);
-    }
-    
-    
-    // MARK: prepareVC
     
     func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
         return CGRect(x: x, y: y, width: width, height: height)
     }
     
-    func prepareUI(){
-        self.addSearchBar()
-        self.addRefreshControl()
-    }
-    
-    func addSearchBar(){
-        if self.searchBar == nil{
-            self.searchBarBoundsY = (self.navigationController?.navigationBar.frame.size.height)! + UIApplication.shared.statusBarFrame.size.height
-            
-            self.searchBar = UISearchBar(frame: CGRectMake(0,self.searchBarBoundsY!, UIScreen.main.bounds.size.width, 44))
-            self.searchBar!.searchBarStyle       = UISearchBarStyle.minimal
-            self.searchBar!.tintColor            = UIColor.white
-            self.searchBar!.barTintColor         = UIColor.white
-            self.searchBar!.delegate             = self;
-            self.searchBar!.placeholder          = "search here";
-            
-            self.addObservers()
-        }
-        
-        if !self.searchBar!.isDescendant(of: self.view){
-            self.view .addSubview(self.searchBar!)
-        }
-    }
    
-    func addRefreshControl(){
-        if (self.refreshControl == nil) {
-            self.refreshControl            = UIRefreshControl()
-            self.refreshControl?.tintColor = UIColor.white
-            self.refreshControl?.addTarget(self, action: #selector(refreshControlAction), for: UIControlEvents.valueChanged)
-        }
-        if !self.refreshControl!.isDescendant(of: self.collectionView!) {
-            self.collectionView!.addSubview(self.refreshControl!)
-        }
-    }
-    
-    func startRefreshControl(){
-        if !self.refreshControl!.isRefreshing {
-            self.refreshControl!.beginRefreshing()
-        }
-    }
-    
-    func addObservers(){
-        let context = UnsafeMutablePointer<UInt8>(bitPattern: 1)
-        self.collectionView?.addObserver(self, forKeyPath: "contentOffset", options: [.new,.old], context: context)
-    }
-    
-    func removeObservers(){
-        self.collectionView?.removeObserver(self, forKeyPath: "contentOffset")
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath! == "contentOffset" {
-            if let collectionV:UICollectionView = object as? UICollectionView {
-                self.searchBar?.frame = CGRectMake(
-                    self.searchBar!.frame.origin.x,
-                    self.searchBarBoundsY! + ( (-1 * collectionV.contentOffset.y) - self.searchBarBoundsY!),
-                    self.searchBar!.frame.size.width,
-                    self.searchBar!.frame.size.height
-                )
-            }
-        }
-    }
 }
 
 
